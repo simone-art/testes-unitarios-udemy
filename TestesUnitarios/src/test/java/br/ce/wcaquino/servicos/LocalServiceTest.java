bb9c086166a6f5a984e3c8ffbde8edab487429f8
simone-art <simoneperiodista3@gmail.com>
2024-02-08 10:05:26 -0300

    Usando o @Rule error-collector

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocalServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocalServiceTest.java
@@ -4,18 +4,24 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
-import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.Assertions;
+
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
 import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
 import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 
 public class LocalServiceTest {
+
+    //Rule, artificio de Junit que permite alterar o comportamento do teste
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
 
     @Test
     public void teste() {
@@ -29,15 +35,20 @@ public class LocalServiceTest {
 
         // Definir a verificação de acordo com o cenário esperado
        //Assertions.assertEquals(locacao.getValor(), 5);
-        Assertions.assertTrue(isMesmaData(locacao.getDataLocacao(), new Date()));
-        Assertions.assertTrue(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+//        Assertions.assertTrue(isMesmaData(locacao.getDataLocacao(), new Date()));
+//        Assertions.assertTrue(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
 
         // AsserThat = verifica que!
         // é uma verificação genérica
-        assertThat(locacao.getValor(), is(equalTo(5.0)));
-        assertThat(locacao.getValor(), is(not(6.0)));
-        assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-        assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+//        assertThat(locacao.getValor(), is(equalTo(5.0)));
+//        assertThat(locacao.getValor(), is(not(6.0)));
+//        assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+//        assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+
+        //Usando o errorCollector pra identificar testes que falham ao executá-los
+        error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+        error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(false));
     }
 }
