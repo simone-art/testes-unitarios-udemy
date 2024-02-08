51716ca3ba52c850f557e45f4349996bed38e17b
simone-art <simoneperiodista3@gmail.com>
2024-02-08 09:37:47 -0300

    Usando o assertThat no teste unitário

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocalServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocalServiceTest.java
@@ -4,10 +4,16 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LocalServiceTest {
 
@@ -22,11 +28,16 @@ public class LocalServiceTest {
         Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
         // Definir a verificação de acordo com o cenário esperado
-        Assertions.assertEquals(locacao.getValor(), 5);
-        Assertions.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        Assertions.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+       //Assertions.assertEquals(locacao.getValor(), 5);
+        Assertions.assertTrue(isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assertions.assertTrue(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
 
-
+        // AsserThat = verifica que!
+        // é uma verificação genérica
+        assertThat(locacao.getValor(), is(equalTo(5.0)));
+        assertThat(locacao.getValor(), is(not(6.0)));
+        assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
     }
 }
